2eda5d86f69248c392e98cda7904f3ae8aa0d9c0
josemavarez11 <josemmr311@gmail.com>
2024-08-12 13:52:32 -0400

    test14 spaced repetition algorithm

--- cards/auxiliar.py
+++ cards/auxiliar.py
@@ -1,53 +1,24 @@
-from datetime import datetime, timezone
-from .serializers import CardSerializer
-import json
-from .models import LearningPhase, LearningStep
-def register_new_card(id_deck, val_card, mea_card):
-    serializer = CardSerializer(data={
-        'id_deck': id_deck,
-        'val_card': val_card,
-        'mea_card': mea_card,
-        'day_added_card': datetime.now(),
-    })
-
-    if serializer.is_valid():
-        serializer.save()
-        return serializer.data
-    else: 
-        return serializer.errors
-    
-def parse_cards_string_to_dict(cards_str):
-    data = json.loads(cards_str)
-    
-    # Cambiar los valores del diccionario a formato con guiones bajos
-    cards_dict = {key: value.replace(' ', '_') for key, value in data.items()}
-    
-    return cards_dict
-
 def evaluate_card(card, id_learning_step, graduating_interval, graduating_max_interval, step):
 
     graduating_max_interval = int(graduating_max_interval)  # Convert to integer
     now = datetime.now(timezone.utc)  # Actualizar el manejo de la fecha y hora
     
     # Convertir campos de fecha y hora a objetos datetime
-    if card['fir_review_card']:
-        card['fir_review_card'] = datetime.fromisoformat(card['fir_review_card'].replace('Z', '+00:00'))
-    if card['las_review_card']:
-        card['las_review_card'] = datetime.fromisoformat(card['las_review_card'].replace('Z', '+00:00'))
+    if card.fir_review_card:
+        card.fir_review_card = datetime.fromisoformat(card.fir_review_card.replace('Z', '+00:00'))
+    if card.las_review_card:
+        card.las_review_card = datetime.fromisoformat(card.las_review_card.replace('Z', '+00:00'))
     
     try:
-        # Asegurar que rev_card sea un entero
-        card['rev_card'] = int(card['rev_card']) if card['rev_card'] is not None else 0
-        
         # Comprobar si es la primera vez que se revisa la carta
-        first_review = card['fir_review_card'] is None
+        first_review = card.fir_review_card is None
     
         if not first_review:
             # Incrementar el contador de revisiones
-            card['rev_card'] += 1
+            card.rev_card = int(card.rev_card) + 1
     
-        if card['day_added_card']:
-            card['day_added_card'] = datetime.fromisoformat(card['day_added_card'].replace('Z', '+00:00'))
+        if card.day_added_card:
+            card.day_added_card = datetime.fromisoformat(card.day_added_card.replace('Z', '+00:00'))
     except Exception as e:
         raise Exception(f"Error al actualizar el contador de revisiones y la fecha de creación de la carta: {str(e)}")
     
@@ -64,7 +35,7 @@ def evaluate_card(card, id_learning_step, graduating_interval, graduating_max_in
     def update_learning_phase(new_phase):
         try:
             new_phase_id = LearningPhase.objects.get(des_learning_phase=new_phase).id
-            card['id_learning_phase'] = new_phase_id
+            card.id_learning_phase = new_phase_id
         except Exception as e:
             raise Exception(f"Error al actualizar la fase de aprendizaje: {str(e)}")
 
@@ -86,79 +57,79 @@ def evaluate_card(card, id_learning_step, graduating_interval, graduating_max_in
     if first_review:
         try:
             # Primer revisión de la carta, Fase de Aprendizaje (LP)
-            card['fir_review_card'] = now
-            card['rev_card'] = 1
+            card.fir_review_card = now
+            card.rev_card = 1
 
             learning_step = LearningStep.objects.get(id=id_learning_step).des_learning_step
 
             if learning_step == "Good":
-                card['las_interval_card'] = step  # Intervalo en minutos
-                card['nex_interval_card'] = step
+                card.las_interval_card = step  # Intervalo en minutos
+                card.nex_interval_card = step
                 update_learning_phase('Graduated Phase')
             elif learning_step == "Hard":
-                card['las_interval_card'] = 1  # 1 minuto
-                card['nex_interval_card'] = 1
+                card.las_interval_card = 1  # 1 minuto
+                card.nex_interval_card = 1
             elif learning_step == "Again":
-                card['las_interval_card'] = 0
-                card['nex_interval_card'] = 0
+                card.las_interval_card = 0
+                card.nex_interval_card = 0
             elif learning_step == 'Easy':
-                card['las_interval_card'] = step  # 10 minutos, transición inmediata a GP
-                card['nex_interval_card'] = step
-                card['eas_factor_card'] = initial_ease * 100
+                card.las_interval_card = step  # 10 minutos, transición inmediata a GP
+                card.nex_interval_card = step
+                card.eas_factor_card = initial_ease * 100
 
-            card['las_review_card'] = now
+            card.las_review_card = now
         except Exception as e:
             raise Exception(f"Error al evaluar la carta en la primera revisión: {str(e)}")
     else:
         try:
             # Cartas en Fase de Aprendizaje (LP) o Graduadas (GP)
-            learning_phase = LearningPhase.objects.get(id=card['id_learning_phase']).des_learning_phase
+            learning_phase = LearningPhase.objects.get(id=card.id_learning_phase).des_learning_phase
             if learning_phase == 'Learning Phase':
                 if learning_step == "Again":
-                    card['lap_card'] = True
-                    card['nex_interval_card'] = 10  # carta caducada, nuevo intervalo fijo a 10 minutos
-                    card['eas_factor_card'] *= 0.8 
+                    card.lap_card = True
+                    card.nex_interval_card = 10  # carta caducada, nuevo intervalo fijo a 10 minutos
+                    card.eas_factor_card *= 0.8 
                 elif learning_step == "Hard":
-                    card['nex_interval_card'] = calculate_new_interval(card['las_interval_card'], 1.2)
-                    card['eas_factor_card'] *= 0.85
+                    card.nex_interval_card = calculate_new_interval(card.las_interval_card, 1.2)
+                    card.eas_factor_card *= 0.85
                 elif learning_step == "Good":
-                    card['nex_interval_card'] = calculate_new_interval(card['las_interval_card'], 2.5)
+                    card.nex_interval_card = calculate_new_interval(card.las_interval_card, 2.5)
                 elif learning_step == "Easy":
-                    card['nex_interval_card'] = calculate_new_interval(card['las_interval_card'], 2.5 * 1.3)
-                    card['eas_factor_card'] += 0.15
+                    card.nex_interval_card = calculate_new_interval(card.las_interval_card, 2.5 * 1.3)
+                    card.eas_factor_card += 0.15
 
-                card['eas_factor_card'] = within_bounds(card['eas_factor_card']) * 100
+                card.eas_factor_card = within_bounds(card.eas_factor_card) * 100
 
             else:
                 # Cuando la carta está en Fase de Aprendizaje (LP)
                 if learning_step == "Hard":
-                    card['nex_interval_card'] = 1  # aparece nuevamente en 1 minuto
+                    card.nex_interval_card = 1  # aparece nuevamente en 1 minuto
                 elif learning_step == "Good":
-                    card['nex_interval_card'] = graduating_interval * 60  # transición a GP, convertir horas a minutos
+                    card.nex_interval_card = graduating_interval * 60  # transición a GP, convertir horas a minutos
                     update_learning_phase('Graduated Phase')
                 elif learning_step == "Again":
-                    card['nex_interval_card'] = 0
+                    card.nex_interval_card = 0
                 elif learning_step == "Easy":
-                    card['nex_interval_card'] = graduating_interval * 60  # transición a GP, convertir horas a minutos
-                    card['eas_factor_card'] = (initial_ease - 0.20) * 100  # inicializando ease
+                    card.nex_interval_card = graduating_interval * 60  # transición a GP, convertir horas a minutos
+                    card.eas_factor_card = (initial_ease - 0.20) * 100  # inicializando ease
 
-                card['las_interval_card'] = card['nex_interval_card']
+                card.las_interval_card = card.nex_interval_card
         except Exception as e:
             raise Exception(f"Error al evaluar la carta en la revisión: {str(e)}")
         
-        card['las_review_card'] = now
+        card.las_review_card = now
 
     try:
         # Actualizar el último paso de aprendizaje
         id_last_learning_step = LearningStep.objects.get(des_learning_step=learning_step).id
-        card['id_last_learning_step'] = id_last_learning_step
+        card.id_last_learning_step = id_last_learning_step
 
         # Calcular tiempos de revisión para cada paso:
         if learning_phase == 'Graduated Phase':
             review_times["Again"] = 10
-            review_times["Hard"] = calculate_new_interval(card['las_interval_card'], 1.2)
-            review_times["Good"] = calculate_new_interval(card['las_interval_card'], 2.5)
-            review_times["Easy"] = calculate_new_interval(card['las_interval_card'], 2.5 * 1.3)
+            review_times["Hard"] = calculate_new_interval(card.las_interval_card, 1.2)
+            review_times["Good"] = calculate_new_interval(card.las_interval_card, 2.5)
+            review_times["Easy"] = calculate_new_interval(card.las_interval_card, 2.5 * 1.3)
         else:
             # Learning Phase
             review_times["Again"] = 0
